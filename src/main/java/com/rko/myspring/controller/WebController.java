bc7c12ffb2ce38e6d21ba3e211da2821b6b05615
Riko-L <ledevedec.eric@gmail.com>
2018-05-22 17:03:32 +0200

    Debut de WebController

--- src/main/java/com/rko/myspring/controller/WebController.java
+++ src/main/java/com/rko/myspring/controller/WebController.java
@@ -1,5 +1,20 @@
 package com.rko.myspring.controller;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WebController {
+
+	private Object message = "Hello World";
+
+	@RequestMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "welcome";
+
+	}
 
 }
