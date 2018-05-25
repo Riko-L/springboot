ea030e5c098515428f0ded7aeb4071649feaa5a0
Riko-L <ledevedec.eric@gmail.com>
2018-05-25 10:15:17 +0200

    Version pour test Heroku

--- src/main/java/com/rko/myspring/model/Message.java
+++ src/main/java/com/rko/myspring/model/Message.java
@@ -14,6 +14,14 @@ public class Message {
     //@NotBlank
     private String content;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
